4095b11bbbd83a769c739b7b090832ad7d0cd7ce
Stavroula Guerrero <4stacyg@optonline.net>
2018-06-16 20:21:48 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,12 +28,12 @@ def adding
   shipping_manifest = {
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
-  "muskets" => 2, "gun powder" => 4
+                  "oil paintings" => 3,
 
+  # add 2 muskets to the shipping_manifest hash below
+  "muskets" => 2,
   # add 4 gun powder to the shipping_manifest hash below
+  "gun powder" => 4}
 
 
 
