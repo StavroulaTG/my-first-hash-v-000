bab489754fe28f855dcc0079399411030c5d63f3
Stavroula Guerrero <4stacyg@optonline.net>
2018-06-16 19:33:24 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  my_hash = {"highlighter"=>"yellow", "whiteout"=>"EZCorrect"}
+  my_hash = {"highlighter" => "yellow", "whiteout" => "EZCorrect"}
 
 end
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
